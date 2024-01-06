b0f386061be5103d04d46b2047d4d92e3c382349
Olexii Bulhakov <bulhakovolexii@gmail.com>
2024-01-06 20:15:08 +0200

    Initial commit for jtd theme

---
+++ docs/_includes/js/custom.js
@@ -0,0 +1,17 @@
+// Script to switch dark and light mode depending on client settings
+window
+  .matchMedia("(prefers-color-scheme: dark)")
+  .addEventListener("change", (event) => {
+    if (event.matches) {
+      jtd.setTheme("dark");
+    } else {
+      jtd.setTheme("light");
+    }
+  });
+
+if (
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: dark)").matches
+) {
+  jtd.setTheme("dark");
+}
