c4fbea080c73f74a51e4c1d2eab952e4f3d5b154
Olexii Bulhakov <bulhakovolexii@gmail.com>
2024-05-13 12:54:12 +0300

    Fixed error with GA in main app

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,13 +1,24 @@
-import { GoogleAnalytics } from '@next/third-parties/google'
 import { InputDataProvider } from "@/app/context/InputDataContext";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
 import { Box, CssBaseline, ThemeProvider } from "@mui/material";
 import theme from "./theme";
+import Script from 'next/script';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="uk">
-      
+      <head>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-N1RH5HL6QP"></Script>
+        <Script id='google-analytics'>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-N1RH5HL6QP');
+          `}
+        </Script>
+      </head>
       <body>
         <AppRouterCacheProvider>
           <ThemeProvider theme={theme}>
@@ -20,7 +31,6 @@ export default function RootLayout({ children }) {
           </ThemeProvider>
         </AppRouterCacheProvider>
       </body>
-      <GoogleAnalytics gaId="G-N1RH5HL6QP" />
     </html>
   );
 }
