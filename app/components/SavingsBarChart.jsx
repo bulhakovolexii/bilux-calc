6bb346dc78c885e671b666aeea1a7ba7e58d404a
Olexii Bulhakov <bulhakovolexii@gmail.com>
2024-07-10 16:24:12 +0300

    Added SavingsChartBar

---
+++ app/components/SavingsBarChart.jsx
@@ -0,0 +1,75 @@
+import { useTheme } from "@emotion/react";
+import { BarChart } from "@mui/x-charts/BarChart";
+
+const chartSetting = {
+  xAxis: [
+    {
+      label: "Енергоспоживання (кВт)",
+      barGapRatio: 0,
+    },
+  ],
+
+  height: 600,
+};
+
+const valueFormatter = (value) => `${value}, кВт·год`;
+
+export default function SavingsBarChart({ data }) {
+  const palette = useTheme().palette;
+
+  const calculateDifferenceInPercentage = (index) => {
+    const item = data[index];
+    return ((1 - item.systemB / item.systemA) * 100).toFixed();
+  };
+
+  return (
+    <BarChart
+      dataset={data.map((item) => ({
+        ...item,
+        offset: Math.min(item.systemA, item.systemB),
+        saving: item.systemA - item.systemB,
+      }))}
+      yAxis={[{ scaleType: "band", dataKey: "month" }]}
+      series={[
+        {
+          dataKey: "systemB",
+          label: "BILUX",
+          color: palette.secondary.main,
+          valueFormatter,
+        },
+        {
+          dataKey: "offset",
+
+          stack: "difference",
+          color: "transparent",
+          valueFormatter: () => null,
+        },
+        {
+          dataKey: "saving",
+          id: "saving",
+          stack: "difference",
+          color: palette.info.main,
+          label: "Потенційна економія",
+          valueFormatter,
+        },
+        {
+          dataKey: "systemA",
+          label: "Користувацька система",
+          color: palette.primary.main,
+          valueFormatter,
+        },
+      ]}
+      layout="horizontal"
+      tooltipLabelFormatter={(value, { dataKey }) => {
+        return dataKey !== "offset" ? value : null;
+      }}
+      barLabel={(item) => {
+        if (item.seriesId === "saving" && item.value > 0) {
+          return `${calculateDifferenceInPercentage(item.dataIndex)} %`;
+        }
+        return null;
+      }}
+      {...chartSetting}
+    />
+  );
+}
--- app/results/page.jsx
+++ app/results/page.jsx
@@ -2,124 +2,53 @@
 
 import Link from "next/link";
 import { useInputData } from "../context/InputDataContext";
-import Building from "../model/Building";
-import monthlyDurationIntervals from "../model/reference-data/monthlyDurationIntervals";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import Building from "../model/Building";
+import monthlyDurationIntervals from "../model/reference-data/monthlyDurationIntervals";
+import SavingsBarChart from "../components/SavingsBarChart";
+import { Container } from "@mui/material";
+
+const biluxSystem = {
+  heatGenerator:
+    "Електричні прилади прямого нагріву: конвектори, поверхневе опалення, променеве опалення, нагрівальний підлоговий кабель",
+  heatingDevices: { type: "Стельові променеві обігрівачі" },
+  controlType: "Двопозиційне регулювання",
+};
+
+const results = (building, biluxBuilding) => {
+  return monthlyDurationIntervals.map(
+    (month) =>
+      new Object({
+        month: month.month,
+        systemA: Math.round(building.energyConsumption(month)),
+        systemB: Math.round(biluxBuilding.energyConsumption(month)),
+      })
+  );
+};
 
 const SuccessfulResult = ({ inputData }) => {
-  const biluxSystem = {
-    heatGenerator:
-      "Електричні прилади прямого нагріву: конвектори, поверхневе опалення, променеве опалення, нагрівальний підлоговий кабель",
-    heatingDevices: { type: "Стельові променеві обігрівачі" },
-    controlType: "Двопозиційне регулювання",
-  };
-  const [building, setBuilding] = useState(new Building(inputData));
-  const [buildingWithBilux, setBuildingWithBilux] = useState(
-    new Building({
-      ...inputData,
-      system: biluxSystem,
-    })
-  );
+  const building = new Building(inputData);
+  const biluxBuilding = new Building({ ...inputData, system: biluxSystem });
   const [indoorTemp, setIndoorTemp] = useState(building.indoorTemperature);
+  const [barData, setBarData] = useState(results(building, biluxBuilding));
   useEffect(() => {
     building.setIndoorTemperature(indoorTemp);
-    buildingWithBilux.setIndoorTemperature(indoorTemp);
+    building.setIndoorTemperature(indoorTemp);
+    setBarData(results(building, biluxBuilding));
   }, [indoorTemp]);
-  const buildingEnergyDemand = monthlyDurationIntervals.map((month) =>
-    building.energyDemand(month)
-  );
-  const buildingEnergyConsumption = monthlyDurationIntervals.map((month) =>
-    building.energyConsumption(month)
-  );
-  const buildingWithBiluxEnergyConsumption = monthlyDurationIntervals.map(
-    (month) => buildingWithBilux.energyConsumption(month)
-  );
 
-  function calculatePercentageDifference(array1, array2) {
-    if (array1.length !== array2.length) {
-      throw new Error("Массивы должны быть одинаковой длины");
-    }
+  console.log(barData);
 
-    let percentageDifferences = [];
-
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] === 0) {
-        // Устанавливаем значение, если деление на ноль
-        percentageDifferences.push(array2[i] === 0 ? 0 : Infinity);
-      } else {
-        let difference = array2[i] - array1[i];
-        let percentageDifference = (difference / array1[i]) * 100;
-        percentageDifferences.push(percentageDifference);
-      }
-    }
-
-    return percentageDifferences;
-  }
-  const economy = calculatePercentageDifference(
-    buildingWithBiluxEnergyConsumption,
-    buildingEnergyConsumption
-  );
   return (
     <>
-      <h1>Результати</h1>
       <Link href="/">На головну</Link>
-      <div>
-        <label id="temp">Кімнатна температура </label>
-        <input
-          type="number"
-          name="temp"
-          id="temp"
-          value={indoorTemp}
-          onChange={(e) => setIndoorTemp(e.target.value)}
-        />
-        <span>
-          &nbsp;Встановлена потужність теплогенератора:
-          {building.estimatedHeatGeneratorPower(-23).toFixed()}, кВт
-        </span>
-      </div>
-      <div style={{ display: "flex", gap: 16 }}>
-        <div>
-          <p>Енергопотреба, кВт·год</p>
-          {buildingEnergyDemand.map((month, index) => (
-            <p key={index}>
-              <span>{monthlyDurationIntervals[index].month}</span>
-              &nbsp;&nbsp;&nbsp;
-              {month.toFixed(2)} {}
-            </p>
-          ))}
-        </div>
-        <div>
-          <p>Енергоспоживання, кВт·год (Користувацька система)</p>
-          {buildingEnergyConsumption.map((month, index) => (
-            <p key={index}>
-              <span>{monthlyDurationIntervals[index].month}</span>
-              &nbsp;&nbsp;&nbsp;
-              {month.toFixed(2)} {}
-            </p>
-          ))}
-        </div>
-        <div>
-          <p>Енергоспоживання, кВт·год (BILUX)</p>
-          {buildingWithBiluxEnergyConsumption.map((month, index) => (
-            <p key={index}>
-              <span>{monthlyDurationIntervals[index].month}</span>
-              &nbsp;&nbsp;&nbsp;
-              {month.toFixed(2)} {}
-            </p>
-          ))}
-        </div>
-        <div>
-          <p>Економія, %</p>
-          {economy.map((month, index) => (
-            <p key={index}>
-              <span>{monthlyDurationIntervals[index].month}</span>
-              &nbsp;&nbsp;&nbsp;
-              {month.toFixed(0)} {}
-            </p>
-          ))}
-        </div>
-      </div>
+      <input
+        type="number"
+        value={indoorTemp}
+        onChange={(e) => setIndoorTemp(+e.target.value)}
+      />
+      <SavingsBarChart data={barData} />
     </>
   );
 };
@@ -130,6 +59,8 @@ export default function Results() {
   return !inputData.city ? (
     router.push("/")
   ) : (
-    <SuccessfulResult inputData={inputData} />
+    <Container maxWidth="lg">
+      <SuccessfulResult inputData={inputData} />
+    </Container>
   );
 }
