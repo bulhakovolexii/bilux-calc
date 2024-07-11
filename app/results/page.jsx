c50e11f8a1847d83f654e4c95059d7ca30d9651a
Olexii Bulhakov <bulhakovolexii@gmail.com>
2024-07-11 12:29:26 +0300

    Added economy calculation to welcome page

--- app/results/page.jsx
+++ app/results/page.jsx
@@ -15,6 +15,7 @@ import {
   Typography,
   Slider,
   Button,
+  TextField,
 } from "@mui/material";
 import Background from "../components/Background";
 import MyAppBar from "../components/MyAppBar";
@@ -45,26 +46,106 @@ const results = (building, biluxBuilding) => {
   });
 };
 
+const getEstimatedPower = (building) => {
+  const fullPower = building.estimatedHeatGeneratorPower(-23);
+  const power = new Object({
+    full: fullPower,
+    specific: (fullPower / building.conditionedArea()) * 1000,
+  });
+
+  return power;
+};
+
+const resourceTypes = [
+  {
+    type: "Горючі корисні копалини тверді",
+    calorificValue: 7.8,
+    units: "кг",
+    defaultRate: 20,
+    rate: 20,
+  },
+  {
+    type: "Горючі корисні копалини скраплені та газоподібні",
+    calorificValue: 9.3,
+    units: "м³",
+    defaultRate: 7.93,
+    rate: 7.93,
+  },
+  {
+    type: "Електроенергія",
+    calorificValue: 1,
+    units: "кВт·год",
+    defaultRate: 4.32,
+    rate: 4.23,
+  },
+  { type: "Солома", calorificValue: 4.3, units: "кг", defaultRate: 1, rate: 1 },
+  {
+    type: "Деревина",
+    calorificValue: 3.9,
+    units: "кг",
+    defaultRate: 2.2,
+    rate: 2.2,
+  },
+  {
+    type: "Централізоване опалення",
+    calorificValue: 1163,
+    units: "Гкал",
+    defaultRate: 1539.5,
+    rate: 1539.5,
+  },
+];
+
 const SuccessfulResult = ({ inputData }) => {
   const building = new Building(inputData);
   const biluxBuilding = new Building({ ...inputData, system: biluxSystem });
   const defaultTemp = building.indoorTemperature;
   const [indoorTemp, setIndoorTemp] = useState(defaultTemp);
   const [estimatedHeatGeneratorPower, setEstimatedHeatGeneratorPower] =
-    useState(building.estimatedHeatGeneratorPower(-23));
+    useState(getEstimatedPower(building));
   const [barData, setBarData] = useState(results(building, biluxBuilding));
+  const [userResource, setUserResource] = useState(
+    resourceTypes.find(
+      (resource) => resource.type === building.heatGenerator().energyResource
+    )
+  );
+  const [electricity, setElectricity] = useState(
+    resourceTypes.find((resource) => resource.type === "Електроенергія")
+  );
 
   useEffect(() => {
     building.setIndoorTemperature(indoorTemp);
     biluxBuilding.setIndoorTemperature(indoorTemp);
     setBarData(results(building, biluxBuilding));
-    setEstimatedHeatGeneratorPower(building.estimatedHeatGeneratorPower(-23));
+    setEstimatedHeatGeneratorPower(getEstimatedPower(building));
   }, [indoorTemp]);
 
   const annualSystemA = barData.reduce((sum, month) => sum + month.systemA, 0);
   const annualSystemB = barData.reduce((sum, month) => sum + month.systemB, 0);
   const savings = annualSystemA - annualSystemB;
   const savingsInPercentage = (1 - annualSystemB / annualSystemA) * 100;
+
+  const handleChangeUserResource = (newRate) => {
+    const newUserResource = { ...userResource, rate: +newRate };
+    setUserResource(newUserResource);
+  };
+
+  const handleChangeElectricity = (newRate) => {
+    const newElectricity = { ...electricity, rate: +newRate };
+    setElectricity(newElectricity);
+    if (userResource.type === "Електроенергія") {
+      setUserResource(newElectricity);
+    }
+  };
+
+  const calculateEconomy = () => {
+    const userCosts =
+      (annualSystemA / userResource.calorificValue) * userResource.rate;
+    const biluxCosts =
+      (annualSystemB / electricity.calorificValue) * electricity.rate;
+    const economy = userCosts - biluxCosts;
+
+    return economy > 0 ? economy : 0;
+  };
 
   const marks = [
     {
@@ -96,8 +177,8 @@ const SuccessfulResult = ({ inputData }) => {
         <Box display="flex" alignItems="flex-start" gap={4}>
           <Slider
             getAriaValueText={valuetext}
-            min={10}
-            max={28}
+            min={marks[0].value}
+            max={marks[2].value}
             defaultValue={indoorTemp}
             onChangeCommitted={(e, newValue) => setIndoorTemp(newValue)}
             step={0.5}
@@ -125,7 +206,8 @@ const SuccessfulResult = ({ inputData }) => {
               Розрахункова потужність системи:
             </Typography>
             <Typography variant="h6" flexGrow={1} align="right">
-              {numberFormatter(estimatedHeatGeneratorPower, 2)}, кВт
+              {numberFormatter(estimatedHeatGeneratorPower.full, 1)}, кВт (
+              {numberFormatter(estimatedHeatGeneratorPower.specific)}, Вт/м²)
             </Typography>
           </Box>
           <Divider />
@@ -154,10 +236,55 @@ const SuccessfulResult = ({ inputData }) => {
               {numberFormatter(savingsInPercentage, 1)}%)
             </Typography>
           </Box>
-          <Divider />
+          <Box>
+            <Typography>Ціна енергоресурсу</Typography>
+            <Stack direction="row" spacing={2}>
+              {userResource.type !== "Електроенергія" && (
+                <TextField
+                  fullWidth
+                  type="number"
+                  inputProps={{ min: 0.01, step: 0.01 }}
+                  label={userResource.type}
+                  InputProps={{
+                    endAdornment: (
+                      <Typography variant="caption">
+                        грн/{userResource.units}
+                      </Typography>
+                    ),
+                  }}
+                  variant="filled"
+                  value={userResource.rate}
+                  onChange={(e) => handleChangeUserResource(e.target.value)}
+                />
+              )}
+              <TextField
+                fullWidth
+                type="number"
+                inputProps={{ min: 0.01, step: 0.01 }}
+                label="Електроенергія"
+                InputProps={{
+                  endAdornment: (
+                    <Typography variant="caption">грн/кВт·год</Typography>
+                  ),
+                }}
+                variant="filled"
+                value={electricity.rate}
+                onChange={(e) => handleChangeElectricity(e.target.value)}
+              />
+            </Stack>
+          </Box>
+          <Box display="flex" gap={1} flexWrap="wrap">
+            <Typography variant="h4">Потенційна економія:</Typography>
+            <Typography variant="h4" flexGrow={1} align="right">
+              {numberFormatter(calculateEconomy(), 0)}, грн/рік
+            </Typography>
+          </Box>
         </Stack>
       </Grid>
       <Grid item xs={12} md={6}>
+        <Typography variant="h4" mb={2}>
+          Розподіл витрат по місяцям
+        </Typography>
         <SavingsBarChart data={barData} />
       </Grid>
     </Grid>
