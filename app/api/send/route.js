f92882260fb82e62a3609ce177e94da9af683be4
Olexii Bulhakov <bulhakovolexii@gmail.com>
2024-07-21 18:50:40 +0300

    Added pdf generation

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -1,4 +1,14 @@
+import EmailTemplate from "@/app/components/EmailTemplate";
 import { Resend } from "resend";
+import { renderToBuffer } from "@react-pdf/renderer";
+import { PdfReport } from "../../components/PdfReport";
+
+export async function generatePdf(inputData, barData) {
+  const pdfBuffer = await renderToBuffer(
+    <PdfReport inputData={inputData} barData={barData} />
+  );
+  return pdfBuffer;
+}
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
@@ -6,11 +16,20 @@ export async function POST(req, res) {
   const { email, inputData, barData } = await req.json();
 
   try {
+    const pdfBuffer = await generatePdf(inputData, barData);
     const { data, error } = await resend.emails.send({
       from: "Bilux-CALC <results@bulhakov.dev>",
       to: [email],
       subject: "Ваш розрахунок готовий!",
-      text: JSON.stringify({ inputData, barData }),
+      react: EmailTemplate({ email }),
+      attachments: [
+        {
+          filename: "report.pdf",
+          content: pdfBuffer.toString("base64"),
+          type: "application/pdf",
+          disposition: "attachment",
+        },
+      ],
     });
 
     if (error) {
