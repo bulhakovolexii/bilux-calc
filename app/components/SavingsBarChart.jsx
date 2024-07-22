26e61d754e1281396b80b88dde47fc810f9f998a
Olexii Bulhakov <bulhakovolexii@gmail.com>
2024-07-22 21:26:23 +0300

    Changed timeout of waiting bar chart animation

--- app/components/SavingsBarChart.jsx
+++ app/components/SavingsBarChart.jsx
@@ -19,7 +19,7 @@ export default function SavingsBarChart({ data, onImageGenerated, height }) {
           onImageGenerated ? onImageGenerated(dataUrl) : null;
         });
       }
-    }, 1000);
+    }, 2000);
   }, []);
 
   const calculateDifferenceInPercentage = (index) => {
