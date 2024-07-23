d787be408e848dcf86bfd32d43b58d1436759248
Olexii Bulhakov <bulhakovolexii@gmail.com>
2024-07-23 21:56:53 +0300

    Added favicon & manifest file

--- app/layout.jsx
+++ app/layout.jsx
@@ -9,6 +9,29 @@ export default function RootLayout({ children }) {
   return (
     <html lang="uk">
       <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#8fc58f" />
+        <meta name="apple-mobile-web-app-title" content="Bilux-CALC" />
+        <meta name="application-name" content="Bilux-CALC" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#fefefe" />
         <Script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-N1RH5HL6QP"
