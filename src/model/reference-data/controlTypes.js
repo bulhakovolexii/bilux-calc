057d51e70bf2869e5e30ce563ad98b94e4b1ae9f
Olexii Bulhakov <bulhakovolexii@gmail.com>
2025-02-27 17:25:30 +0200

    docs: add links to all reference data

--- src/model/reference-data/controlTypes.js
+++ src/model/reference-data/controlTypes.js
@@ -1,3 +1,9 @@
+/*
+Складова загального рівня ефективності, яка враховує регулювання температури приміщення
+Джерело: Наказ мінрегіону № 169 від 11.07.2018 “Про затвердження Методики визначення енергетичної ефективності будівель”
+URL: https://bulhakov.dev/bilux-calc/appendixes/appendix-h/
+**/
+
 const controlTypes = [
   { type: "Відсутнє", temperatureControlEfficiency: 0.86 },
   { type: "Двопозиційне регулювання", temperatureControlEfficiency: 0.93 },
